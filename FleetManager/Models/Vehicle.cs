e720f74ddc22df4e6cfa09ca2dcc7db442d840df
kornel <gg@g.k>
2026-03-16 13:13:37 +0100

    rozpoczecie prac nad wygladem aplikacji

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -2,5 +2,9 @@ namespace FleetManager.Models;
 
 public class Vehicle
 {
-    
+    public string Marka { get; set; } = string.Empty;
+    public string Model { get; set; } =  string.Empty;
+    public int RokProdukcji { get; set; } = 1990;
+    public string IloscPaliwa { get; set; } = "5%";
+    public string StatusPojazdu { get; set; } = "W garażu";
 }
--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,92 @@
-﻿namespace FleetManager.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text.Json;
+using FleetManager.Models;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace FleetManager.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private const string Sciezka = "Assets/vehicles.json";
+
+    private static readonly JsonSerializerOptions JsonOptions = new() { WriteIndented = true };
+
+    public ObservableCollection<Vehicle> Vehicles { get; } = [];
+    
+    [Reactive] public string NowaMarka {get; set;} = string.Empty;
+    [Reactive] public string NowyModel {get; set;} = string.Empty;
+    [Reactive] public int NowyRokProdukcji { get; set; } = 0000;
+    [Reactive] public string NowaIloscPaliwa {get; set;} = "5%";
+    [Reactive] public string NowyStatus {get; set;} = "W garażu";
+    
+    public ReactiveCommand<Unit, Unit> AddCommand { get; }
+    public ReactiveCommand<Unit, Unit> SaveCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        LoadVehicles();
+        
+        AddCommand = ReactiveCommand.Create(AddVehicles);
+        SaveCommand = ReactiveCommand.Create(SaveToJson);
+    }
+
+    private void AddVehicles()
+    {
+        Vehicles.Add(new Vehicle
+        {
+            Marka = NowaMarka,
+            Model = NowyModel,
+            RokProdukcji = NowyRokProdukcji,
+            StatusPojazdu = NowyStatus,
+            IloscPaliwa = NowaIloscPaliwa
+        });
+    }
+
+    private void SaveToJson()
+    {
+        try
+        {
+            File.WriteAllText(Sciezka, JsonSerializer.Serialize(Vehicles, JsonOptions));
+            Console.WriteLine("Zapisane");
+        }
+        catch
+            (Exception exception) when (exception is
+                                            IOException or
+                                            UnauthorizedAccessException or
+                                            JsonException)
+        {
+            Console.WriteLine($"Save File Error {exception.Message}");
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"Unexpected error (report this!): {exception.Message}");
+        }
+    }
+
+    private void LoadVehicles()
+    {
+        if (!File.Exists(Sciezka)) return;
+        try
+        {
+            var jsonData = File.ReadAllText(Sciezka);
+            var list = JsonSerializer.Deserialize<List<Vehicle>>(jsonData);
+            Vehicles.Clear();
+            if (list != null) return;
+            foreach (var vehicle in list)
+            {
+                Vehicles.Add(vehicle);
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"Wczytanie pliku nie powiodło się {exception}");
+        }
+    }
+    
 }
