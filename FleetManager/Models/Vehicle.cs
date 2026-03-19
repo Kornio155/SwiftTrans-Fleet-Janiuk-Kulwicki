306f46543f0ef0ee2823e42f257cda167aff1aec
kornel <gg@g.k>
2026-03-19 11:10:36 +0100

    Dodanie kolorów do wyswietlania pojazdow

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -5,6 +5,20 @@ public class Vehicle
     public string Marka { get; set; } = string.Empty;
     public string Model { get; set; } =  string.Empty;
     public int RokProdukcji { get; set; } = 1990;
-    public string IloscPaliwa { get; set; } = "5%";
+    public int IloscPaliwa { get; set; } = 5;
     public string StatusPojazdu { get; set; } = "W garażu";
+    
+    public string KolorTla
+    {
+        get
+        {
+            if (IloscPaliwa < 15)
+                return "#8a2b34";        
+
+            if (IloscPaliwa <= 30)
+                return "#c49618";          
+
+            return "#347527";       
+        }
+    }
 }
